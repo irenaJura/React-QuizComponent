0bb108de146355481a1a5e5c8a27bfb4d034aa61
Irena Jurasek <irenaskydive@gmail.com>
2020-02-09 10:05:09 +0100

    Add Event Handlers

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,19 +4,23 @@ import { QuizEnd } from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
 
-export class Quiz extends Component {
+ export class Quiz extends Component {
     constructor(props) {
         super(props);
         this.state = { quiz_position: 1 }
-    } 
+    }
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1}
+        });    
+    }
     render() {
-        const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_questions.length)
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
             <div>
-                {isQuizEnd ? <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
-        );
+        )
   }
 }
 
